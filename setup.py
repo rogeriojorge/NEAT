a1e723acc4ea95ce1cad93a034c2e2690cb6b47c
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2022-04-08 17:10:15 +0100

    Removed VMEC to add new forked version with slightly modified CMakeLists.txt

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
-import sys
+# import sys
 import json
 import pathlib
 import os
 from skbuild import setup
 
-print("system.platform is {}".format(sys.platform))
-if (sys.platform == "darwin"):
-    from distutils import sysconfig
-    vars = sysconfig.get_config_vars()
-    vars['LDSHARED'] = vars['LDSHARED'].replace('-bundle', '-dynamiclib')
+# print("system.platform is {}".format(sys.platform))
+# if (sys.platform == "darwin"):
+#     from distutils import sysconfig
+#     vars = sysconfig.get_config_vars()
+#     vars['LDSHARED'] = vars['LDSHARED'].replace('-bundle', '-dynamiclib')
 
 fldr_path = pathlib.Path(__file__).parent.absolute()
-
 with open(os.path.join(fldr_path, 'cmake_config_file.json')) as fp:
     d = json.load(fp)
 
-class EmptyListWithLength(list):
-    def __len__(self):
-        return 1
+# class EmptyListWithLength(list):
+#     def __len__(self):
+#         return 1
 
 setup(cmake_args=d['cmake_args'])
