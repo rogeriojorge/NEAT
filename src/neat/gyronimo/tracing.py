466c3b9e7c096351c5276854a60465422080deec
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2022-04-20 22:43:56 +0100

    Added new gyronimo related classes for particles, orbits and fields

--- src/neat/gyronimo/tracing.py
+++ src/neat/gyronimo/tracing.py
@@ -1,34 +1,35 @@
 import logging
 import numpy as np
-from fields import stellna_qs
+from .fields import stellna_qs
 from neatpp import gc_solver_qs
-from util.constants import PROTON_MASS, ELEMENTARY_CHARGE, MU_0
+from ..util.constants import PROTON_MASS, ELEMENTARY_CHARGE, MU_0
 from scipy.interpolate import CubicSpline as spline
 
 logger = logging.getLogger(__name__)
 
-class particle():
+class charged_particle():
     r"""
     Class that contains the physics information of a
     given charged particle, as well as its position
     and velocity
     """
-    def __init__(self) -> None:
-        self.charge = 1
-        self.rhom = 1
-        self.mass = 1
-        self.Lambda = 0.8
-        self.energy = 4e4
-        self.r0 = 0.05
-        self.theta0 = 0
-        self.phi0 = 0
+    def __init__(self, charge=1, rhom=1, mass=1, Lambda=0.4,
+                 energy=4e4, r0=0.05, theta0=0, phi0=0) -> None:
+        self.charge = charge
+        self.rhom = rhom
+        self.mass = mass
+        self.energy = energy
+        self.Lambda = Lambda
+        self.r0 = r0
+        self.theta0 = theta0
+        self.phi0 = phi0
 
-    def params(self):
+    def gyronimo_parameters(self):
         return self.charge, self.rhom, self.mass,\
                self.Lambda, self.energy, self.r0,\
                self.theta0, self.phi0
 
-def orbit(stel, params, B20real, nsamples, Tfinal):
+class particle_orbit():
     r"""
     Interface function with the C++ executable NEAT. Receives a pyQSC instance
     and outputs the characteristics of the orbit.
@@ -38,78 +39,63 @@ def orbit(stel, params, B20real, nsamples, Tfinal):
             r0,theta0,phi0,charge,rhom,mass,Lambda,energy,nsamples,Tfinal
         B20real (bool): True if a constant B20real should be used, False otherwise
     """
-    gyronimo_field = stellna_qs(stel, B20real)
-    gyronimo_particle = particle()
-    sol = np.array(
-        gc_solver_qs(
-            gyronimo_field.params(),
-            gyronimo_particle.params(),
-            nsamples,
-            Tfinal
+    def __init__(self, particle, field, nsamples=500, Tfinal=1000) -> None:
+        solution = np.array(
+            gc_solver_qs(
+                *field.gyronimo_parameters(),
+                *particle.gyronimo_parameters(),
+                nsamples,
+                Tfinal
+            )
         )
-    )
+        self.gyronimo_parameters = solution
 
-    time = sol[:, 0]
-    r_pos = sol[:, 1]
-    theta_pos = sol[:, 2]
-    varphi = sol[:, 3]
-    
-    nu = stel.varphi - stel.phi
-    nu_spline_of_varphi = spline(
-        np.append(stel.varphi, 2 * np.pi / stel.nfp),
-        np.append(nu, nu[0]),
-        bc_type="periodic",
-    )
+        self.time = solution[:, 0]
+        self.r_pos = solution[:, 1]
+        self.theta_pos = solution[:, 2]
+        self.varphi = solution[:, 3]
+        
+        nu = field.varphi - field.phi
+        nu_spline_of_varphi = spline(
+            np.append(field.varphi, 2 * np.pi / field.nfp),
+            np.append(nu, nu[0]),
+            bc_type="periodic",
+        )
 
-    phi_pos = varphi - nu_spline_of_varphi(varphi)
-    energy_parallel = sol[:, 4]
-    energy_perpendicular = sol[:, 5]
-    total_energy = energy_parallel + energy_perpendicular
-    Bfield = sol[:, 6]
-    v_parallel = sol[:, 7]
-    rdot = sol[:, 8]
-    thetadot = sol[:, 9]
-    phidot = sol[:, 10]
-    vppdot = sol[:, 11]
+        self.phi_pos = self.varphi - nu_spline_of_varphi(self.varphi)
+        self.energy_parallel = solution[:, 4]
+        self.energy_perpendicular = solution[:, 5]
+        self.total_energy = self.energy_parallel + self.energy_perpendicular
+        self.Bfield = solution[:, 6]
+        self.v_parallel = solution[:, 7]
+        self.rdot = solution[:, 8]
+        self.thetadot = solution[:, 9]
+        self.phidot = solution[:, 10]
+        self.vppdot = solution[:, 11]
+
+        self.p_phi = canonical_angular_momentum(particle, field, self.r_pos, self.v_parallel, self.Bfield)
+
+def canonical_angular_momentum(particle, field, r_pos, v_parallel, Bfield):
 
     m_proton = PROTON_MASS
     e = ELEMENTARY_CHARGE
     mu0 = MU_0
-    Valfven = stel.Bbar / np.sqrt(mu0 * params["rhom"] * m_proton * 1.0e19)
+    Valfven = field.Bbar / np.sqrt(mu0 * particle.rhom * m_proton * 1.0e19)
 
     p_phi1 = (
-        params["mass"]
+        particle.mass
         * m_proton
         * v_parallel
         * Valfven
-        * (stel.G0 + r_pos**2 * (stel.G2 + (stel.iota - stel.iotaN) * stel.I2))
+        * (field.G0 + r_pos**2 * (field.G2 + (field.iota - field.iotaN) * field.I2))
         / Bfield
-        / stel.Bbar
+        / field.Bbar
     )
     
-    p_phi2 = params["charge"] * e * r_pos**2 * stel.Bbar / 2 * stel.iotaN
+    p_phi2 = particle.charge * e * r_pos**2 * field.Bbar / 2 * field.iotaN
     p_phi = p_phi1 - p_phi2
 
-    return [
-        time,
-        r_pos,
-        theta_pos,
-        phi_pos,
-        total_energy,
-        params["theta0"],
-        params["phi0"],
-        params["Lambda"],
-        params["energy"],
-        p_phi,
-        rdot,
-        thetadot,
-        phidot,
-        vppdot,
-        v_parallel,
-        Bfield,
-        varphi,
-    ]
-
+    return p_phi
 
 class trace_particles():
     r"""
---
+++ tests/gyronimo/test_particle_tracing.py
@@ -0,0 +1,24 @@
+import logging
+import unittest
+import numpy as np
+from neat.gyronimo.fields import stellna_qs
+from neat.gyronimo.tracing import charged_particle, particle_orbit
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class NEATtests(unittest.TestCase):
+    def test_orbit_energy_momentum(self):
+        """
+        Test that an orbit traced in a quasisymmetric stellarator
+        conserves energy and angular momentum
+        """
+        n_samples = 100
+        Tfinal = 1000
+        precision = 6
+
+        g_field = stellna_qs.from_paper(1)
+        g_particle = charged_particle()
+        g_orbit = particle_orbit(g_particle, g_field, nsamples=n_samples, Tfinal = Tfinal)
+        np.testing.assert_array_almost_equal(g_orbit.total_energy, [g_orbit.total_energy[0]]*(n_samples+1), decimal=precision)
+        np.testing.assert_array_almost_equal(g_orbit.p_phi, [g_orbit.p_phi[0]]*(n_samples+1), decimal=precision)
