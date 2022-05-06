dc7145519149988cad1c479a959780beacd036c1
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2022-05-06 12:38:20 +0100

    Changes to pass CI tests

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -7,13 +7,13 @@ from neat.tracing import charged_particle_ensemble
 
 class NEATtests(unittest.TestCase):
     def test_optimization(self):
-        r_surface_max = 0.1
+        r_max = 0.1
         r_initial = 0.09
         energy = 1e4
 
         g_field = stellna_qs.from_paper(4, nphi=131)
         g_particle = charged_particle_ensemble(r0=r_initial, energy=energy)
-        optimizer = optimize_loss_fraction(g_field, g_particle, r_surface_max)
+        optimizer = optimize_loss_fraction(g_field, g_particle, r_max)
         initial_loss_fraction = optimizer.loss_fraction.J()
         optimizer.run(nIterations=4)
         final_loss_fraction = optimizer.loss_fraction.J()
--- tests/test_particle_tracing.py
+++ tests/test_particle_tracing.py
@@ -44,7 +44,7 @@ class NEATtests(unittest.TestCase):
         """
         nthreads_array = [1, 2, 4]
         nthreads = 8
-        r_surface_max = 0.13
+        r_max = 0.13
 
         g_field = stellna_qs.from_paper(4)
         g_particle = charged_particle_ensemble()
@@ -57,7 +57,7 @@ class NEATtests(unittest.TestCase):
         #     "The OpenMP parallelization is not working",
         # )
         g_orbits = particle_ensemble_orbit(g_particle, g_field, nthreads=nthreads)
-        loss_fraction = g_orbits.loss_fraction(r_surface_max=r_surface_max)
+        loss_fraction = g_orbits.loss_fraction(r_max=r_max)
         self.assertTrue(
             all(
                 loss_fraction[i] <= loss_fraction[i + 1]
@@ -78,9 +78,9 @@ class NEATtests(unittest.TestCase):
         return total_time
 
     def test_plotting(self):
-        n_samples = 100
+        n_samples = 1000
         Tfinal = 0.0001
-        g_field = stellna_qs.from_paper(1)
+        g_field = stellna_qs.from_paper(4)
         g_particle = charged_particle()
         g_orbit = particle_orbit(g_particle, g_field, nsamples=n_samples, Tfinal=Tfinal)
         g_orbit.plot(show=True)
