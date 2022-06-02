e2afb2bfc1d8e6e8c95c1d23e6ad088c55b75722
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2022-06-02 16:40:51 +0100

    Slightly better initial condition for optimization test

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -9,11 +9,11 @@ from neat.tracing import charged_particle_ensemble
 
 class NEATtests(unittest.TestCase):
     def test_optimization(self):
-        r_max = 0.1
+        r_max = 0.07
         r_initial = 0.03
         energy = 1e4
 
-        g_field = stellna_qs.from_paper(4, nphi=131)
+        g_field = stellna_qs.from_paper(4, B0=4, nphi=131)
         g_particle = charged_particle_ensemble(r0=r_initial, energy=energy)
         optimizer = optimize_loss_fraction_skeleton(g_field, g_particle, r_max)
         initial_loss_fraction = optimizer.residual.J()
