92fea108053727491145db6a0f57a5bd3b2c38b5
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2023-02-03 10:37:46 +0000

    Now working with ensemble due to position of operator overloading

--- src/neat/fields.py
+++ src/neat/fields.py
@@ -29,11 +29,11 @@ from scipy.io import netcdf
 
 from neatpp import (
     gc_solver,
-    # gc_solver_ensemble,
+    gc_solver_ensemble,
     gc_solver_qs,
-    # gc_solver_qs_ensemble,
+    gc_solver_qs_ensemble,
     gc_solver_qs_partial,
-    # gc_solver_qs_partial_ensemble,
+    gc_solver_qs_partial_ensemble,
     vmectrace,
 )
 
